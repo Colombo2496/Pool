3ecb45044c95e0d112e434eae911147a8d0a11b9
Colombo2496 <juancutrera@gmail.com>
2018-06-05 15:22:10 +1000

    Working needs to be added the popup window

--- cueballdecorator.cpp
+++ cueballdecorator.cpp
@@ -24,6 +24,7 @@ void CueBallDecorator::mousePressed(QMouseEvent *event)
     {
         clicked = true;
         mousePos = QVector2D(event->pos());
+        placeCue = false;
     }else if(placeCue){
         mousePos = QVector2D(event->pos());
     }
@@ -53,6 +54,7 @@ void CueBallDecorator::mouseReleased(QMouseEvent *event)
         }else
         {
             qDebug() << "must be inside the baulk";
+            qDebug() << mousePos;
         }
     }else if(clicked)
     {
